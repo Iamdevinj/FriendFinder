790f1574cf7504db881dc69bdcf756fa0f2b903f
Devin Jenkins <dvnjenks@gmail.com>
2019-07-20 00:18:00 -0700

    added array to friends.js page

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+var freinds = [
+  {
+    name: "Ahmed",
+    photo:
+      "https://en.wikipedia.org/wiki/Aziz_Ansari#/media/File:Aziz_Ansari_2012_Shankbone.JPG",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Awkwafina",
+    photo: "https://www.thewrap.com/wp-content/uploads/2018/11/Awkwafina.jpg",
+    scores: [3, 2, 1, 4, 5, 1, 2, 3, 5, 2]
+  },
+  {
+    name: "Beyonce",
+    photo:
+      "https://www.hellomagazine.com/imagenes/fashion/hfm/2019071675378/beyonce-yellow-dress-style-trick-meghan-markle-lion-king/0-366-352/beyonce-lion-king-dress-z.jpg",
+    scores: [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+  },
+  {
+    name: "Ryan Gosling",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Ryan_Gosling_in_2018.jpg/1024px-Ryan_Gosling_in_2018.jpg",
+    scores: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5]
+  },
+  {
+    name: "Khloe Kardashian",
+    photo:
+      "https://www.cheatsheet.com/wp-content/uploads/2019/07/Khloe-Kardashian-nose-job-1024x683.jpg",
+    scores: [1, 3, 2, 4, 5, 3, 1, 4, 2, 5]
+  },
+  {
+    name: "Kanye West",
+    photo:
+      "https://pixel.nymag.com/imgs/daily/vulture/2017/08/01/1-kanye-west.w700.h467.2x.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  }
+];
