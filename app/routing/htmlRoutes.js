17696184607ae6fbf743b7983f6c34e7126db1d3
Devin Jenkins <dvnjenks@gmail.com>
2019-07-20 13:10:11 -0700

    fixed routing on multiple pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,6 +4,10 @@ module.exports = function(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/survey.html"));
   });
+  app.get("/friends", function(req, res) {
+    res.sendFile(path.join(__dirname + "/../data/friends.js"));
+  });
+  
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
